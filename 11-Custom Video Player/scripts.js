8cd125e801636ebaaf5946770d10acc06332f014
Hilal38 <hhilalkocak@gmail.com>
2024-02-08 09:07:01 +0300

    skip video when click back and forward skip buttons

--- 11-Custom Video Player/scripts.js
+++ 11-Custom Video Player/scripts.js
@@ -4,7 +4,8 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip');
+const skipButtons = player.querySelectorAll('[data-skip]');
+
 const playPause = player.querySelector('.play-pause');
 const ranges = player.querySelectorAll('.player__slider');
 
@@ -18,8 +19,15 @@ function updateButton(){
     toggle.textContent = this.paused ? '►' : '❚ ❚';
 
 }
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
 // Hook up the events listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach((button) => button.addEventListener('click', skip))
