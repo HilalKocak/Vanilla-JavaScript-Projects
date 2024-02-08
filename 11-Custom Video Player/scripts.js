0f179c3c24ec35169a6d6a1c78cf7651dcbf7d55
Hilal38 <hhilalkocak@gmail.com>
2024-02-08 10:02:43 +0300

    move video by dragging

--- 11-Custom Video Player/scripts.js
+++ 11-Custom Video Player/scripts.js
@@ -57,4 +57,10 @@ skipButtons.forEach((button) => button.addEventListener('click', skip));
 ranges.forEach((range) => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach((range) => range.addEventListener('mousemove', handleRangeUpdate));
 
+let mousedown = false;
+
 progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e)=> mousedown && scrub(e));
+
+progress.addEventListener('mousedown', ()=> mousedown = true);
+progress.addEventListener('mouseup', ()=> mousedown = false);
