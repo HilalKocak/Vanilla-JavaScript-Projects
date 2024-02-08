941207dee180cb7ed2f9459461895b82fbd07433
Hilal38 <hhilalkocak@gmail.com>
2024-02-08 08:56:23 +0300

    change icon as play and paused when video play and pause

--- 11-Custom Video Player/scripts.js
+++ 11-Custom Video Player/scripts.js
@@ -3,7 +3,7 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const toggled = player.querySelector('.toggle');
+const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelector('[data-skip');
 const playPause = player.querySelector('.play-pause');
 const ranges = player.querySelectorAll('.player__slider');
@@ -13,5 +13,13 @@ function togglePlay() {
     const method = video.paused ? 'play' : 'pause';
     video[method]();
 }
+
+function updateButton(){
+    toggle.textContent = this.paused ? '►' : '❚ ❚';
+
+}
 // Hook up the events listeners
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
