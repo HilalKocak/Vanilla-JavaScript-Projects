3612b097f491f85d11e3a71cdcd093f263c9bd10
Hilal38 <hhilalkocak@gmail.com>
2024-02-16 08:50:50 +0300

    Get video stream and show to user

--- 19-Webcam Fun/scripts.js
+++ 19-Webcam Fun/scripts.js
@@ -3,3 +3,14 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    .then(localMediaStream => {
+        console.log(localMediaStream);
+        video.srcObject = localMediaStream;
+        video.play(); 
+    })
+}
+
+getVideo();
