97678924cee57f94da1a742e2ca0c0e37cda6a26
Hilal38 <hhilalkocak@gmail.com>
2024-02-24 07:59:25 +0300

    Add event listener to start timer when submit

--- 29-Countdown Timer/scripts.js
+++ 29-Countdown Timer/scripts.js
@@ -52,3 +52,13 @@ function startTimer(){
 
 }
 buttons.forEach(button => button.addEventListener('click', startTimer))
+
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const mins = this.minutes.value;
+    console.log(mins)
+    timer(mins * 60);
+    this.reset();
+
+
+})
