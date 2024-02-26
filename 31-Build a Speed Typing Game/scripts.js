ba1841c0761cef4bdb7a9fe9a3323faf56029a74
Hilal38 <hhilalkocak@gmail.com>
2024-02-26 08:45:39 +0300

    Change color of display text to input typing

--- 31-Build a Speed Typing Game/scripts.js
+++ 31-Build a Speed Typing Game/scripts.js
@@ -1,4 +1,29 @@
 const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
+const quoteDisplay = document.getElementById('quoteDisplay')
+const quoteInput = document.querySelector('.quote-input')
+
+
+quoteInput.addEventListener('input', ()=> {
+    console.log('changed')
+    const arrayQuote = quoteDisplay.querySelectorAll('span')
+    const arrayValue = quoteInput.value.split('')
+
+    arrayQuote.forEach((characterSpan, index)=> {
+        const character = arrayValue[index]
+        if(character == null){ // user didnt type yet
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+        }else if(character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        }else{
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+        }
+
+    })
+
+})
 
 function getRandomQuote(){
     return fetch(RANDOM_QUOTE_API_URL)
@@ -8,7 +33,13 @@ function getRandomQuote(){
 
 async function renderNewQuote(){
     const quote = await getRandomQuote()
-    console.log(quote)
+    quoteDisplay.innerHTML = '';
+    quoteInput.value = null;
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplay.appendChild(characterSpan)
+    })
 }
 
 renderNewQuote()
