0266ab28c8a467ed09898cc627a87f6b8bdf1f21
Hilal38 <hhilalkocak@gmail.com>
2024-03-03 21:34:34 +0300

    Add tast written in input tolist container

---
+++ 33-To Do List/script.js
@@ -0,0 +1,12 @@
+const inputBox = document.getElementById('input-box')
+const listContainer = document.getElementById('list-container')
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert('You must write something')
+    }else{
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value
+        listContainer.appendChild(li)
+    }
+}
