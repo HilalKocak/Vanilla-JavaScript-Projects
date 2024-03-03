79179f767de40277040bf066988209274e648013
Hilal38 <hhilalkocak@gmail.com>
2024-03-03 21:56:36 +0300

    Add delete button and style

--- 33-To Do List/script.js
+++ 33-To Do List/script.js
@@ -8,5 +8,9 @@ function addTask(){
         let li = document.createElement("li")
         li.innerHTML = inputBox.value
         listContainer.appendChild(li)
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
     }
+    inputBox.value = ''
 }
