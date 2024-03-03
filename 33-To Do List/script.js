643da0f229f8d49674c796435fa25928c4cac8e3
Hilal38 <hhilalkocak@gmail.com>
2024-03-03 22:04:51 +0300

    Mark tasks as checked and remove

--- 33-To Do List/script.js
+++ 33-To Do List/script.js
@@ -14,3 +14,13 @@ function addTask(){
     }
     inputBox.value = ''
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+}, false)
+
