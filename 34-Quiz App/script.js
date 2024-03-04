a151dfe8613cd906a2bea6dbb70af8ad3854a210
Hilal38 <hhilalkocak@gmail.com>
2024-03-04 09:36:50 +0300

    Add next button and play again

--- 34-Quiz App/script.js
+++ 34-Quiz App/script.js
@@ -89,11 +89,12 @@ function selectAnswer(e) {
     const isCorrect = selectedBtn.dataset.correct === "true"
     if(isCorrect){
         selectedBtn.classList.add("correct")
+        score++
     }else{
         selectedBtn.classList.add("incorrect")
     }
     Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === true){
+        if(button.dataset.correct === "true"){
             button.classList.add("correct")
         }
         button.disabled = true
@@ -101,4 +102,28 @@ function selectAnswer(e) {
 
     nextButton.style.display = "block"
 }
+
+function showScore() {
+    resetState()
+    questionElement.innerHTML = `Your scored ${score} out of ${questions.length}!`
+    nextButton.innerHTML = "Play again"
+    nextButton.style.display = "block"
+}
+
+function handleNextButton() {
+    currentQuestionIndex++
+    if(currentQuestionIndex < questions.length){
+        showQuestion()
+    }else{
+        showScore()
+    }
+}
+
+nextButton.addEventListener("click", ()=> {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton()
+    }else{
+        startQuiz()
+    }
+})
 startQuiz()
